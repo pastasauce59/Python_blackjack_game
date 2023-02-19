3fb2ffb5a21288bd0f155a6b658ec81977ecfd61
Michael Primavera <pastasauce59@gmail.com>
2023-02-18 23:12:33 -0500

    refactored functions to simplify code by removing rand_card function and moving it into now deal_card function instead of deal_cardS, created function to calculate score and remove the for-in loops that were calculating that, also removed and changed some variable names.

--- blackjack.py
+++ blackjack.py
@@ -16,38 +16,46 @@ print(logo)
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-def rand_card():
-        return random.choice(cards)
+# def rand_card():
+#         return random.choice(cards)
         
-def draw_cards():
-    user_cards = [rand_card(), rand_card()]
-    comp_cards = [rand_card()]
-    return [user_cards, comp_cards]
+def deal_card():
+    # user_cards = [rand_card(), rand_card()]
+    # comp_cards = [rand_card(), rand_card()]
+    # return [user_cards, comp_cards]
+    return random.choice(cards)
+
+def calculate_score(list_of_cards):
+    return sum(list_of_cards)
 
 begin = input("Do you want to play a game of Blackjac: Type 'y' or 'n': ")
 if begin == 'y':
-    your_hand = draw_cards()[0]
-    your_score = 0
-    comp_hand = draw_cards()[1]
-    comp_score = 0
+    user_cards = []
+    computer_cards = []
+    for n in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
 
-    for card in your_hand:
-        your_score += card
-    for card in comp_hand:
-        comp_score += card
+    your_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
 
-    print(f"Your cards: {your_hand}, current score: {your_score}")
-    print(f"Computer's first card: {comp_hand[0]}")
-    
+    # for card in your_hand:
+    #     your_score += card
+    # for card in comp_hand:
+    #     comp_score += card
+
+    print(f"Your cards: {user_cards}, current score: {calculate_score(user_cards)}")
+    print(f"Computer's first card: {computer_cards[0]}")
+
     draw_another = input("Type 'y' to get another card, type 'n' to pass: ")
     if draw_another == 'y':
         print('Drawing another card...')
     else:
-        print(f"Your final hand: {your_hand}, final score: {your_score}")
-        print(f"Computer's final hand: {comp_hand}, final score: {comp_score}")
-        if your_score > comp_score:
+        print(f"Your final hand: {user_cards}, final score: {your_score}")
+        print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
+        if your_score > computer_score:
             print("You win!")
-        elif your_score == comp_score:
+        elif your_score == computer_score:
             print("Draw game.")
         else:
             print("Computer wins the game.")
