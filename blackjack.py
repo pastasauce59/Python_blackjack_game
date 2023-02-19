4137f5fd41671d49ed6e7b0f3c0a981a6887d3fc
Michael Primavera <pastasauce59@gmail.com>
2023-02-19 14:23:35 -0500

    created a while loop to continue dealing the computer cards while it's total score is less than 17, modified the if and elif statements in end_game function to decide who the winner based on final scores and if they are over/under 21, created keep_playing bool and a while loop to ask the user if they want to keep playing blackjack after a game ends.

--- blackjack.py
+++ blackjack.py
@@ -28,49 +28,48 @@ def end_game():
 
     print(f"Your final hand: {user_cards}, final score: {your_score}")
     print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
-    if your_score > computer_score:
+    if your_score > computer_score and your_score <=21:
+        print("You win!")
+    elif computer_score > 21:
         print("You win!")
     elif your_score == computer_score:
         print("Draw game.")
-    else:
+    elif computer_score > your_score and computer_score <= 21:
+        print("Computer wins the game.")
+    elif your_score > 21:
         print("Computer wins the game.")
 
-begin = input("Do you want to play a game of Blackjac: Type 'y' or 'n': ")
-if begin == 'y':
-    user_cards = []
-    computer_cards = []
-    for n in range(2):
-        user_cards.append(deal_card())
-        computer_cards.append(deal_card())
+keep_playing = True
+while keep_playing:
+    begin = input("Do you want to play a game of Blackjac: Type 'y' or 'n': ")
+    if begin == 'y':
+        user_cards = []
+        computer_cards = []
+        for n in range(2):
+            user_cards.append(deal_card())
+            computer_cards.append(deal_card())
 
-    print(f"Your cards: {user_cards}, current score: {calculate_score(user_cards)}")
-    print(f"Computer's first card: {computer_cards[0]}")
+        print(f"Your cards: {user_cards}, current score: {calculate_score(user_cards)}")
+        print(f"Computer's first card: {computer_cards[0]}")
 
-    user_draws_another = True
-    while user_draws_another:
-        if calculate_score(user_cards) <= 21:
-            draw_another = input("Type 'y' to get another card, type 'n' to pass: ")
-            if draw_another == 'y':
-                user_cards.append(deal_card())
-                print(f"Your cards: {user_cards}, current score: {calculate_score(user_cards)}")
-                print(f"Computer's first card: {computer_cards[0]}")
+        while calculate_score(computer_cards) < 17:
+            computer_cards.append(deal_card())
+
+        user_draws_another = True
+        while user_draws_another:
+            if calculate_score(user_cards) <= 21:
+                draw_another = input("Type 'y' to get another card, type 'n' to pass: ")
+                if draw_another == 'y':
+                    user_cards.append(deal_card())
+                    print(f"Your cards: {user_cards}, current score: {calculate_score(user_cards)}")
+                    print(f"Computer's first card: {computer_cards[0]}")
+                else:
+                    end_game()
+                    user_draws_another = False
             else:
-                user_draws_another = False
                 end_game()
-        else:
-            # user_draws_another = False
-            # your_score = calculate_score(user_cards)
-            # computer_score = calculate_score(computer_cards)
-
-            # print(f"Your final hand: {user_cards}, final score: {your_score}")
-            # print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
-            # if your_score > computer_score:
-            #     print("You win!")
-            # elif your_score == computer_score:
-            #     print("Draw game.")
-            # else:
-            #     print("Computer wins the game.")
-            user_draws_another = False
-            print(f"You overdrew your hand and lose! Your final score {calculate_score(user_cards)}")
-
-    
+                print(f"You overdrew your hand and lose!")
+                user_draws_another = False
+    else:
+        keep_playing = False
+        print("Thanks for playing!")
