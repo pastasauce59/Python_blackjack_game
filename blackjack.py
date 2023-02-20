447d65439b860f04dc257679041586584a1cb134
Michael Primavera <pastasauce59@gmail.com>
2023-02-19 21:49:00 -0500

    added a check for ace card and check for blackjack in 'calculate_score' function, remade if-elif statement checks for 'end_game' function for all likely outcomes,added an if statement to check if computer has an ace before dealing more cards, added an if statement to the while loop of 'user_draws_another' to check for blackjack.

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,4 @@
-############### Our Blackjack House Rules #####################
+############### Blackjack House Rules #####################
 
 ## The deck is unlimited in size. 
 ## There are no jokers. 
@@ -20,7 +20,18 @@ def deal_card():
     return random.choice(cards)
 
 def calculate_score(list_of_cards):
-    return sum(list_of_cards)
+    #Checking for Ace
+    if sum(list_of_cards) > 21:
+        for card in list_of_cards:
+            if card == 11:
+                list_of_cards.remove(11)
+                list_of_cards.append(1)
+    
+    if list_of_cards[0] + list_of_cards[1] == 21:
+        #Checking for Blackjack
+        return 0
+    else:
+        return sum(list_of_cards)
 
 def end_game():
     your_score = calculate_score(user_cards)
@@ -28,23 +39,35 @@ def end_game():
 
     print(f"Your final hand: {user_cards}, final score: {your_score}")
     print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
-    if your_score > computer_score and your_score <=21:
-        print("You win!")
-    elif computer_score > 21:
-        print("You win!")
-    elif your_score == computer_score:
-        print("Draw game.")
-    elif computer_score > your_score and computer_score <= 21:
-        print("Computer wins the game.")
+    
+    if computer_score == 0:
+        print("Computer wins with a Blackjack! 🃏 🤖")
+    elif your_score == 0:
+        print("You win with a Blackjack! 🃏 😃")
     elif your_score > 21:
+        print("You went over and lose!")
+    elif computer_score > 21:
+        print("Computer went over. You win!")
+    elif your_score == computer_score:
+        print("Draw game, there is no winner.")
+    elif your_score > computer_score:
+        print("You win!")
+    elif computer_score > your_score:
         print("Computer wins the game.")
 
 keep_playing = True
 while keep_playing:
     begin = input("Do you want to play a game of Blackjac: Type 'y' or 'n': ")
     if begin == 'y':
+        import os
+        os.system('clear')
         user_cards = []
         computer_cards = []
+
+        #For testing Blackjack
+        # user_cards = [11,10]
+        # computer_cards = [10,11]
+
         for n in range(2):
             user_cards.append(deal_card())
             computer_cards.append(deal_card())
@@ -52,23 +75,27 @@ while keep_playing:
         print(f"Your cards: {user_cards}, current score: {calculate_score(user_cards)}")
         print(f"Computer's first card: {computer_cards[0]}")
 
-        while calculate_score(computer_cards) < 17:
-            computer_cards.append(deal_card())
+        if calculate_score(computer_cards) != 0:
+            while calculate_score(computer_cards) < 17:
+                computer_cards.append(deal_card())
 
         user_draws_another = True
         while user_draws_another:
-            if calculate_score(user_cards) <= 21:
+            if calculate_score(user_cards) == 0:
+                end_game()
+                user_draws_another = False
+            elif calculate_score(user_cards) <= 21:
                 draw_another = input("Type 'y' to get another card, type 'n' to pass: ")
                 if draw_another == 'y':
                     user_cards.append(deal_card())
-                    print(f"Your cards: {user_cards}, current score: {calculate_score(user_cards)}")
+                    user_score = calculate_score(user_cards)
+                    print(f"Your cards: {user_cards}, current score: {user_score}")
                     print(f"Computer's first card: {computer_cards[0]}")
                 else:
                     end_game()
                     user_draws_another = False
             else:
                 end_game()
-                print(f"You overdrew your hand and lose!")
                 user_draws_another = False
     else:
         keep_playing = False
